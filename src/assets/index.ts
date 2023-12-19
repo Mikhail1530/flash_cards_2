0a7411befdbba5cb200df262105af067af45f035
Eugen <vasilenkoevgen88@gmail.com>
2023-12-19 17:02:09 +0200

    Delete unnecessary asset

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,4 @@
 export { default as ArrowDownIcon } from './icons/arrowDownIcon'
-export { default as Card } from './icons/card'
 export { default as CheckBoxIcon } from './icons/checkBoxIcon'
 export { default as HeaderIcon } from './icons/headerIcon'
 export { default as IconClose } from './icons/iconClose'
