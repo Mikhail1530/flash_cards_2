f8d18822e4cefc1299007c21319f9baf4c671eb6
Eugen <vasilenkoevgen88@gmail.com>
2023-12-21 14:48:37 +0200

    Add Page SingUp

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import { Button } from '@/components/ui/button'
-import { TabSwitcher } from '@/components/ui/tabSwitcher'
-import {SignIn} from "@/features/auth/signIn";
-import PageLogin from "@/pages/page-login";
+import PageLogin from '@/pages/page-login'
 
 export function App() {
   return (
     <div>
-      {/*<Button variant={'primary'}>Link Button</Button>*/}
-      {/*<TabSwitcher buttonsName={['first-btn']} />*/}
-        <PageLogin/>
+      <PageLogin />
     </div>
   )
 }
