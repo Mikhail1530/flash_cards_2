0c8ea1cd0a436f1491d94c53794ea76912a08a6a
Eugen <vasilenkoevgen88@gmail.com>
2023-12-21 12:41:10 +0200

    Minor fix style for card height

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { Button } from '@/components/ui/button'
 import { TabSwitcher } from '@/components/ui/tabSwitcher'
+import {SignIn} from "@/features/auth/signIn";
+import PageLogin from "@/pages/page-login";
 
 export function App() {
   return (
     <div>
-      <Button variant={'primary'}>Link Button</Button>
-      <TabSwitcher buttonsName={['first-btn']} />
+      {/*<Button variant={'primary'}>Link Button</Button>*/}
+      {/*<TabSwitcher buttonsName={['first-btn']} />*/}
+        <PageLogin/>
     </div>
   )
 }
