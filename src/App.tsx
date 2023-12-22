fb26a33389275d4365474458be61f2a1929922a2
Mikhail1530 <michael.nfnr@gmail.com>
2023-12-22 13:24:26 +0400

    Merge and push

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,3 @@
-import { Button } from '@/components/ui/button'
-import { TabSwitcher } from '@/components/ui/tabSwitcher'
-
 export function App() {
-  return (
-    <div>
-      <Button variant={'primary'}>Link Button</Button>
-      <TabSwitcher buttonsName={['first-btn']} />
-    </div>
-  )
+  return <div></div>
 }
