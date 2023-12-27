63895bbace449aba283c93516af47aac1b19b46d
Mikhail1530 <michael.nfnr@gmail.com>
2023-12-27 20:52:05 +0400

    CheckBox fixed

--- src/components/ui/checkBox/checkBox.tsx
+++ src/components/ui/checkBox/checkBox.tsx
@@ -1,4 +1,4 @@
-import { ComponentPropsWithoutRef, FC } from 'react'
+import { ElementRef, forwardRef } from 'react'
 
 import { CheckBoxIcon } from '@/assets'
 import { Typography } from '@/components/ui/typography'
@@ -7,21 +7,32 @@ import cx from 'clsx'
 
 import s from './checkBox.module.scss'
 type CheckBoxProps = {
+  checked?: boolean
+  className?: string
   label?: string
-} & ComponentPropsWithoutRef<typeof Checkbox.Root>
-export const CheckBox: FC<CheckBoxProps> = ({ className, label, ...rest }) => {
-  return (
-    <form className={s.box}>
-      <div className={s.checkBoxWrapper}>
-        <Checkbox.Root className={cx(s.checkboxRoot, className)} {...rest}>
-          <Checkbox.Indicator>
-            <CheckBoxIcon className={s.iconCheckBox} />
-          </Checkbox.Indicator>
-        </Checkbox.Root>
-      </div>
-      <Typography className={s.label} variant={'body2'}>
-        {label}
-      </Typography>
-    </form>
-  )
+  onCheckedChange?: () => void
 }
+export const CheckBox = forwardRef<ElementRef<typeof Checkbox.Root>, CheckBoxProps>(
+  ({ checked, className, label, onCheckedChange, ...rest }, ref) => {
+    return (
+      <div className={s.box}>
+        <div className={s.checkBoxWrapper}>
+          <Checkbox.Root
+            className={cx(s.checkboxRoot, className)}
+            ref={ref}
+            {...rest}
+            checked={checked}
+            onCheckedChange={onCheckedChange}
+          >
+            <Checkbox.Indicator>
+              <CheckBoxIcon className={s.iconCheckBox} />
+            </Checkbox.Indicator>
+          </Checkbox.Root>
+        </div>
+        <Typography className={s.label} variant={'body2'}>
+          {label}
+        </Typography>
+      </div>
+    )
+  }
+)
