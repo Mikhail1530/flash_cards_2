279167a0794da0ce8cbacd446e50d6ff12260285
Eugen <vasilenkoevgen88@gmail.com>
2024-01-17 12:38:53 +0200

    Minor

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Provider } from 'react-redux'
 
 import HeaderIcon from '@/assets/icons/headerIcon'
 import { Header } from '@/components/ui/header'
+import { RadioButtons } from '@/components/ui/radio-buttons/index'
 import { Router } from '@/router'
 import { store } from '@/services/store'
 
@@ -14,6 +15,13 @@ export function App() {
         logoLink={'https://www.google.com/imghp?hl=en'}
       ></Header>
       <Router />
+      <RadioButtons
+        option={[
+          { isDisabled: false, name: 'Option1', value: 1 },
+          { isDisabled: false, name: 'Option2', value: 1 },
+        ]}
+        variant={'Enable'}
+      />
     </Provider>
   )
 }
